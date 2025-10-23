2dc543e3092b4e0c040c347ba523ccd9e9ad21ea
seongmincho33 <seongmincho33@gmail.com>
2025-10-23 16:50:02 +0900

    food-facility빌드 성공

--- food-facility/src/main/java/com/mini/mini_2/facility/domain/entity/FacilityEntity.java
+++ food-facility/src/main/java/com/mini/mini_2/facility/domain/entity/FacilityEntity.java
@@ -1,15 +1,10 @@
 package com.mini.mini_2.facility.domain.entity;
 
-import com.mini.mini_2.rest_area.domain.entity.RestAreaEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,14 +15,11 @@ import lombok.ToString;
 
 @Entity
 @Table(name = "facility")
-
 @Builder
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(exclude = {"restArea"})
-
 public class FacilityEntity {
 
     // PK
@@ -37,12 +29,8 @@ public class FacilityEntity {
     private Integer facilityId;
 
     // FK(rest_area_id)
-    @ManyToOne(fetch = FetchType.LAZY,
-               optional = false)
-    @JoinColumn(name = "rest_area_id")      
-    private RestAreaEntity restArea;          
+    private Integer restAreaId;
 
-    
     @Column(length = 50)
     private String name; 
     
