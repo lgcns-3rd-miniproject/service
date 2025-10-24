fb463bddc4ccfa313046e6ad4f54414bdad55259
j2hxxx <77jihyun77@naver.com>
2025-10-24 16:55:46 +0900

    fix: application.yml instance 추가

--- openai/src/main/java/com/mini/mini_2/client/food/FoodClient.java
+++ openai/src/main/java/com/mini/mini_2/client/food/FoodClient.java
@@ -11,8 +11,8 @@ import com.mini.mini_2.client.food.domain.FoodRequestDTO;
 import com.mini.mini_2.client.food.domain.FoodResponseDTO;
 
 @FeignClient(
-    name = "food-service", 
-    url = "${GATEWAY_URL:http://service-restarea-unit-1:8080}"
+    name = "food-service"
+    //url = "${GATEWAY_URL:http://service-restarea-unit-1:8080}"
 )
 public interface FoodClient {
     @PostMapping("/api/v1/mini/food/create")
--- openai/src/main/java/com/mini/mini_2/client/restarea/RestAreaClient.java
+++ openai/src/main/java/com/mini/mini_2/client/restarea/RestAreaClient.java
@@ -9,8 +9,8 @@ import com.mini.mini_2.client.restarea.domain.RestAreaResponseDTO;
 
 
 @FeignClient(
-    name = "restarea-service", 
-    url = "${GATEWAY_URL:http://service-restarea-1:8080}"
+    name = "restarea-service" 
+    //url = "${GATEWAY_URL:http://service-restarea-1:8080}"
 )
 
 public interface RestAreaClient {
