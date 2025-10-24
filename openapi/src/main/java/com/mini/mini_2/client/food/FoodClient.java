47e0bddf4e7ac2d49480f774db91478997a708d0
junhee95 <jhjang.vdrc@gmail.com>
2025-10-24 13:31:56 +0900

    commit

--- openapi/src/main/java/com/mini/mini_2/client/food/FoodClient.java
+++ openapi/src/main/java/com/mini/mini_2/client/food/FoodClient.java
@@ -1,7 +1,9 @@
 package com.mini.mini_2.client.food;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mini.mini_2.client.food.domain.FoodRequestDTO;
@@ -11,10 +13,13 @@ import com.mini.mini_2.client.food.domain.FoodResponseDTO;
 
 @FeignClient(
     name = "food-service", 
-    url = "${GATEWAY_URL:http://service-food-1:8080}"
+    url = "${GATEWAY_URL:http://service-restarea-unit-1:8080}"
 )
 public interface FoodClient {
     
+    @GetMapping("/api/v1/mini/food/lists")
+    List<FoodResponseDTO> findAll();
+
     @PostMapping("/api/v1/mini/food/create")
     FoodResponseDTO create(FoodRequestDTO foodRequestDTO);
 
